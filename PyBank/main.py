94e7b479fc99222f24d332bf6e33a4671aaa6aa3
Neil7777 <148916909+Neil7777@users.noreply.github.com>
2023-11-15 01:49:43 -0500

    update pybank

--- PyBank/main.py
+++ PyBank/main.py
@@ -9,7 +9,7 @@ input_file = Path("PyBank", "Resources", "budget_data.csv")
 #create variables for the specific rows
 totalmonths = []
 totalPnL = []
-mounthlyPnLchange = []
+monthlyPnLchange = []
 
 #open cvs file
 with open(input_file,newline="", encoding="utf-8") as budget:
@@ -26,4 +26,27 @@ with open(input_file,newline="", encoding="utf-8") as budget:
         totalmonths.append(row[0])
         totalPnL.append(int(row[1]))
 
-       # print (totalmonths,totalPnL)
+        #print (totalmonths,totalPnL)
+
+    for i in range(len(totalPnL)-1):
+
+        monthlyPnLchange.append(totalPnL[i+1]-totalPnL[1])
+
+
+max_profit = max(monthlyPnLchange)
+max_loss = min(monthlyPnLchange)
+
+print(max_loss)
+print(max_profit)
+
+max_increase_month = monthlyPnLchange.index(max(monthlyPnLchange)) +1
+max_decrease_month = monthlyPnLchange.index(min(monthlyPnLchange)) +1
+
+
+print("Financial Analysis")
+print("-------------------------------------")
+print(f"Total Month: {len(totalmonths)}")
+print(f"Total: ${sum(totalPnL)}")
+print(f"Average Change: {round(sum(monthlyPnLchange)/len(monthlyPnLchange),2)}")
+print(f"Greatest Increase in Profits: {totalmonths[max_increase_month]} (${(str(max_profit))})")
+print(f"Greatest Decrease in Profits: {totalmonths[max_decrease_month]} (${(str(max_loss))})")
